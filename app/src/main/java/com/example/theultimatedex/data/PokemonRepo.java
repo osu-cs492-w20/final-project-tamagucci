d7798272faf779d8f41ebc7aa8177220ea008d70
Kira <jirouxk@oregonstate.edu>
2020-03-03 15:17:53 -0800

    Pushing small items. Mostly to get different message for repo.

--- app/src/main/java/com/example/theultimatedex/data/PokemonRepo.java
+++ app/src/main/java/com/example/theultimatedex/data/PokemonRepo.java
@@ -1,11 +1,16 @@
 package com.example.theultimatedex.data;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class PokemonRepo implements Serializable {
 
+    public int id;
+    public String name;
+    public ArrayList<BaseStats> stats;
+
     // Info to retrieve from API of each pokemon
-    // species {}
+    // species {} -- actually no there's just a place for name
     //      - name
     // sprites {}
     //      - front_default
